eb477829c267e737e75fb49e8c16a7dd9ad9c530
Andreia Barboza <andreiabarboza07@gmail.com>
2026-01-03 00:03:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const musicBtn = document.querySelector(".music-btn");
+
+music.addEventListener("play", () => {
+    musicBtn.textContent = "⏸";
+});
+
+music.addEventListener("pause", () => {
+    musicBtn.textContent = "♫";
+});
+
+function toggleMusic() {
+    music.paused ? music.play() : music.pause();
+}
